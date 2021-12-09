f5d0d4d2b2ce0bd04f2611fc52a582eb5bb2dd56
Ali Alhashimi <libmacali@gmail.com>
2021-12-09 15:03:00 +0400

    Added Bootcamp CRUD

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
 const express = require('express');
-const dotenv = require('dotenv'); 
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+
+//Router files
+const bootcamps = require('./routes/bootcamps');
 
 //Load Env Vars
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: './config/config.env' });
+
+//Connect to Database
+connectDB();
 
 const app = express();
 
+//Body Parser 
+app.use(express.json());
+
+//Dev Loggin middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, () => {
-    console.log(`App running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}!`);
+const server = app.listen(PORT, () => {
+  console.log(
+    `App running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}!`
+  .yellow.bold);
 });
 
+//Handle UnHandled Rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //Close Server and Exist Process
+  server.close(() => process.exit(1));
+});
